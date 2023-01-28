c7f5c38ba22f2929cfdaa0ae221e8d99115fd551
Alara Kalfazade <alarakalfazade@alaras-mbp.devices.brown.edu>
2023-01-28 13:27:55 -0500

    img and title

--- front-end/scare-the-bears/src/App.js
+++ front-end/scare-the-bears/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Navbar from './Components/Navbar';
 
 function App() {
+
+  const backgroundImg = require("./media/My project.png");
   return (
     <div className="grid-container">
       <div className="navbar-container">
         <Navbar></Navbar>
       </div>
-
       <div className="image-container">
-
+        <img src={backgroundImg}></img>
+        <h1>Scare The Bears</h1>
+        
       </div>
     </div>
   );
